5acad88df912e35b873eb7a3a6167861e9f1497e
Leandro Paredes <leandroparedes6@gmail.com>
2020-03-21 00:38:42 -0300

    vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-    <div id="app">
-        <router-view/>
-    </div>
+    <v-app>
+        <v-container>
+            <router-view></router-view>
+        </v-container>
+    </v-app>
 </template>
+
+<script>
+export default {
+    name: 'App'
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+    router,
+    vuetify,
+    render: function (h) { return h(App) }
+}).$mount('#app');
