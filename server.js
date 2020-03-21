4f1738e76524103fe180b5616aa59d83d569fc1b
Leandro Paredes <leandroparedes6@gmail.com>
2020-03-21 15:34:31 -0300

    stuff

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + "/dist/"));
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get(/.*/, function (req, res) {
+    res.sendfile(__dirname + "/dist/index.html");
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`Server started on ${port}`));
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,12 @@
 
 <script>
 export default {
-    name: 'home'
+    name: 'home',
+    created () {
+        const url = 'https://covid-api.mmediagroup.fr/v1/history?country=Global&status=confirmed';
+        this.axios.get(url).then(res => {
+            console.log(res);
+        });
+    }
 }
 </script>
