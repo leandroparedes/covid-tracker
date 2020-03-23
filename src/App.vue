5875ce85c08627f27e4f2180757496a482e11318
Leandro Paredes <leandroparedes6@gmail.com>
2020-03-23 13:55:20 -0300

    app bar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
     <v-app>
-        <v-container class="my-8 px-md-12">
-            <router-view></router-view>
-        </v-container>
+        <v-app-bar app dense>
+            <router-link to="/" tag="div" style="cursor:pointer" class="v-toolbar__title">Covid-T</router-link>
+
+            <v-spacer></v-spacer>
+
+            <router-link to="/compare" tag="div" style="cursor:pointer">
+                <v-icon>mdi-chart-areaspline-variant</v-icon> <span class="ml-2">Comparar paises</span>
+            </router-link>
+        </v-app-bar>
+
+        <v-content>
+            <v-container class="my-8 px-md-12">
+                <router-view></router-view>
+            </v-container>
+        </v-content>
     </v-app>
 </template>
 
