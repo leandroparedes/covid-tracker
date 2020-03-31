0eefa428f4c71ec5507a61ea0d4e9c523362769a
Leandro Paredes <leandroparedes6@gmail.com>
2020-03-30 23:49:32 -0300

    related links component

---
+++ src/components/timeline/RelatedLinks.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-list v-if="relatedLinks" dense>
+            <v-subheader class="text-uppercase">
+            {{ $vuetify.lang.t('$vuetify.relatedLinks') }}
+        </v-subheader>
+
+        <v-list-item-group color="primary">
+            <v-list-item
+                v-for="(link, index) in relatedLinks"
+                :key="index"
+            >
+                <v-list-item-content>
+                    <a :href="link.link_url" target="_blank">{{ link.link_text }}</a>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list-item-group>
+    </v-list>
+    <div v-else class="text-center">
+        <div class="subtitle-1 pt-5">
+            {{ $vuetify.lang.t('$vuetify.noRelatedLinks') }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RelatedLinks',
+    props: ['relatedLinks']
+}
+</script>
+
+<style>
+
+</style>
