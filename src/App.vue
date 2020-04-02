00ea2a00b44017e3273ee90306db6b4b0def5e59
Leandro Paredes <leandroparedes6@gmail.com>
2020-04-02 18:46:52 -0300

    compare button

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,10 @@
             <v-btn icon large to="/search">
                 <v-icon>fas fa-search</v-icon>
             </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn icon large to="/compare">
+                <v-icon>fas fa-chart-bar</v-icon>
+            </v-btn>
         <v-content>
             <v-container>
                 <router-view :key="$route.fullPath"></router-view>
