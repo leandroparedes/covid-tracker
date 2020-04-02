e7ad9bb081d4fa6c1539877a0f094d54f2595961
Leandro Paredes <leandroparedes6@gmail.com>
2020-04-01 21:45:39 -0300

    timeline placeholder

---
+++ src/components/placeholders/Timeline.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <v-card class="heading mx-auto mb-6"></v-card>
+        <v-row class="mb-6">
+            <v-col
+                sm="6" offset-sm="6"
+                lg="4" offset-lg="8"
+            >
+                <v-card class="datepicker"></v-card>
+            </v-col>
+        </v-row>
+    
+    <v-timeline :dense="dense">
+        <v-timeline-item color="secondary" v-for="i in 3" :key="i">
+            <v-card class="timeline-item"></v-card>
+        </v-timeline-item>
+    </v-timeline>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TimelinePlaceholder',
+    computed: {
+        dense: function () {
+            return this.$vuetify.breakpoint.name == 'xs'
+                    || this.$vuetify.breakpoint.name == 'sm'
+                    || this.$vuetify.breakpoint.name == 'md';
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .heading {
+        height: 60px;
+        width: 80%;
+    }
+    .datepicker {
+        height: 50px;
+    }
+    .timeline-item {
+        height: 400px;
+    }
+</style>
--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -1,58 +1,61 @@
 <template>
     <div>
-        <div
-            class="font-weight-black text-center text-uppercase mb-8"
-            :class="displayClass"
-        >
-            {{ $vuetify.lang.t('$vuetify.timelineTitle') }}
-        </div>
-
-        <v-row>
-            <v-col
-                sm="6" offset-sm="6"
-                lg="4" offset-lg="8"
-                class="d-flex"
+        <div v-if="! loading">
+            <div
+                class="font-weight-black text-center text-uppercase mb-8"
+                :class="displayClass"
             >
-                <datepicker @go-to-date="handleGoToDate"></datepicker>
-                
-                <v-btn
-                    fab
-                    @click="goToBookmarked"
-                    v-if="$store.state.bookmark"
-                    class="ml-4"
-                    :title="$vuetify.lang.t('$vuetify.goToBookmark')"
+                {{ $vuetify.lang.t('$vuetify.timelineTitle') }}
+            </div>
+
+            <v-row>
+                <v-col
+                    sm="6" offset-sm="6"
+                    lg="4" offset-lg="8"
+                    class="d-flex"
                 >
-                    <v-icon color="primary">fas fa-bookmark</v-icon>
+                    <datepicker @go-to-date="handleGoToDate"></datepicker>
+                    
+                    <v-btn
+                        fab
+                        @click="goToBookmarked"
+                        v-if="$store.state.bookmark"
+                        class="ml-4"
+                        :title="$vuetify.lang.t('$vuetify.goToBookmark')"
+                    >
+                        <v-icon color="primary">fas fa-bookmark</v-icon>
+                    </v-btn>
+                </v-col>
+            </v-row>
+
+            <v-timeline :dense="timelineDense">
+                <v-timeline-item
+                    v-for="(situation, index) in situations.data"
+                    :key="index"
+                >
+                    <template v-slot:opposite>
+                        <highlights :highlights="situation.highlights"></highlights>
+                    </template>
+                    <situation-card :situation="situation"></situation-card>
+                </v-timeline-item>
+            </v-timeline>
+
+            <div
+                v-if="situations.links && situations.links.next"
+                class="text-center"
+            >
+                <v-btn
+                    @click="loadMore"
+                    :loading="loadingMore"
+                    text
+                    x-large
+                >
+                    {{ $vuetify.lang.t('$vuetify.loadMore') }}
                 </v-btn>
-            </v-col>
-        </v-row>
-
-        <v-timeline :dense="timelineDense">
-            <v-timeline-item
-                v-for="(situation, index) in situations.data"
-                :key="index"
-            >
-                <template v-slot:opposite>
-                    <highlights :highlights="situation.highlights"></highlights>
-                </template>
-                <situation-card :situation="situation"></situation-card>
-            </v-timeline-item>
-        </v-timeline>
-
-        <div
-            v-if="situations.links && situations.links.next"
-            class="text-center"
-        >
-            <v-btn
-                @click="loadMore"
-                :loading="loadingMore"
-                text
-                x-large
-            >
-                {{ $vuetify.lang.t('$vuetify.loadMore') }}
-            </v-btn>
+            </div>
         </div>
-
+        <timeline-placeholder v-else></timeline-placeholder>
+        
         <v-overlay :value="loading">
             <v-progress-circular indeterminate size="64"></v-progress-circular>
         </v-overlay>
@@ -64,9 +67,16 @@ import SituationCard from '@/components/timeline/SituationCard.vue';
 import Highlights from '@/components/timeline/Highlights.vue';
 import Datepicker from '@/components/timeline/Datepicker.vue';
 
+import TimelinePlaceholder from '@/components/placeholders/Timeline.vue';
+
 export default {
     name: 'Timeline',
-    components: { SituationCard, Highlights, Datepicker },
+    components: {
+            SituationCard,
+            Highlights,
+            Datepicker,
+            TimelinePlaceholder
+        },
     data: function () {
         return {
             situations: [],
