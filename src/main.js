ffe7f61d91ff234897fb336fde3051efbc260e20
Leandro Paredes <leandroparedes6@gmail.com>
2020-04-02 02:56:23 -0300

    formatting. cleanup, deps

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
-Vue.config.productionTip = false
+Vue.use(VueAxios, axios);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+    router,
+    store,
+    render: function (h) { return h(App) }
+}).$mount('#app');
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,17 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    plugins: [createPersistedState()],
+    state: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    modules: {
+    }
+});
